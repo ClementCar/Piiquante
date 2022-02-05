989860f3c832bfa494f8629a59905eb61c0de608
Clement Carcenac <clementcarcenac@gmail.com>
2022-02-05 13:13:14 +0100

    Ajout de like/dislikes / ajout du readme

---
+++ README.md
@@ -0,0 +1,32 @@
+# ![desktop reservia](/frontend/assets/images/flame.png)  HOTS TAKES
+
+Construisez une API sécurisée pour une application d'avis gastronomiques
+
+## Problématique
+
+- Créer un serveur node.js
+- Construire un parcour utilisateur
+- Construire la route sauce de l'API ( ajouter, modifier, supprimer, afficher toutes les sauces, afficher une sauce)
+- Construire la route like / dislike
+
+## Contenu
+
+- Une page Signup / Login 
+- Une page d'acceuil contenant toutes les sauces de l'API
+- Une page de description d'une sauce choisi
+
+## Languages et outils
+
+- Language JavaScript
+- Utilisation de node.js
+- Utilisation de express
+- Utilisation de MongoDB Atlas
+
+## Notes 
+
+Pour utiliser l'api :
+- Cloner le repository 
+- Ouvrir le terminal, se placer dans le dossier backend
+- effectuer la commande **npm install** puis **nodemon server**
+- Ouvrir le terminal, se placer dans le dossier frontend
+- effectuer la commande **npm install** puis **npm start**
--- backend/controllers/sauce.js
+++ backend/controllers/sauce.js
@@ -2,7 +2,7 @@ const Sauce = require('../models/Sauce');
 const fs = require('fs');
 
 
-// création de 
+// création de sauce
 exports.createSauce = (req, res, next) => {
     const sauceObject = JSON.parse(req.body.sauce);
     delete sauceObject._id;
@@ -15,22 +15,25 @@ exports.createSauce = (req, res, next) => {
         .catch(error => res.status(400).json({ error })); 
 };
 
+// avoir toutes les sauces de la base de données
 exports.getAllSauce = (req, res, next) => {
     Sauce.find()
         .then(sauces => res.status(200).json(sauces))
         .catch(error => res.status(400).json({ error }))
 };
 
+// avoir une sauce
 exports.getOneSauce = (req, res, next) => {
     Sauce.findOne({ _id: req.params.id })
         .then(sauce => res.status(200).json(sauce))
         .catch(error => res.status(404).json({ error }));
 };
 
+// modifier une sauce
 exports.modifySauce = (req, res, next) => {
     const sauceObject = req.file ?
     {
-        ...JSON.parse(req.body.thing),
+        ...JSON.parse(req.body.sauce),
         imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
     } : { ...req.body };
     Sauce.updateOne({ _id: req.params.id }, { ...sauceObject, _id: req.params.id })
@@ -38,6 +41,7 @@ exports.modifySauce = (req, res, next) => {
         .catch(error => res.status(400).json({ error }));
 };
 
+// supprimer une sauce
 exports.deleteSauce = (req, res, next) => {
     Sauce.findOne({ _id: req.params.id })
       .then(sauce => {
@@ -49,4 +53,36 @@ exports.deleteSauce = (req, res, next) => {
         });
       })
       .catch(error => res.status(500).json({ error }));
-  };
+};
+
+// aimer ou non une sauce
+exports.likeSauce = (req, res, next) => {
+    if ( req.body.like === 0 ) {
+        Sauce.findOne({ _id: req.params.id})
+          .then(sauce => {
+              if (sauce.usersLiked.includes(req.body.userId)) {
+                  // $inc: Incrémente la valeur du champ de la quantité spécifiée
+                  // $pull: Supprime tous les éléments de tableau qui correspondent à une requête spécifiée
+                  Sauce.updateOne({ _id: req.params.id}, { $inc: { likes: -1 }, $pull: { usersLiked: req.body.userId }})
+                    .then(() => res.status(200).json({ message: 'Like supprimé '}))
+                    .catch(error => res.status(400).json({ error }));
+              } else if (sauce.usersDisliked.includes(req.body.userId)) {
+                  Sauce.updateOne({ _id: req.params.id}, { $inc: { likes: 1 }, $pull: { usersDisliked: req.body.userId }})
+                    .then(() => res.status(200).json({ message: 'Dislike supprimé '}))
+                    .catch(error => res.status(400).json({ error }));
+              }
+          })
+          .catch(error => res.status(400).json({ error }));
+    }
+    if ( req.body.like === 1 ) {
+        // push: Ajoute un élément à un tableau
+        Sauce.updateOne({ _id: req.params.id }, {$inc: { likes: 1}, $push: { usersLiked: req.body.userId }}) 
+            .then(() => res.status(200).json({ message: 'Sauce aimé '}))
+            .catch(error => res.status(400).json({ error }));
+    }
+    if( req.body.like === -1 ) {
+        Sauce.updateOne({ _id: req.params.id }, {$inc: { likes: -1 }, $push: { usersDisliked: req.body.userId }}) 
+            .then(() => res.status(200).json({ message: 'Sauce non aimé '}))
+            .catch(error => res.status(400).json({ error }));
+    }
+};
